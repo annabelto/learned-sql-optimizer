ff6980d8f69280e661bd0eedb892da60a011a973
YasasviPeruvemba <yasasvi.peruvemba@gmail.com>
2024-04-20 16:08:08 -0500

    Add experimentation code for TPCH

--- TPC-H V3.0.1/dbgen/load_queries.sh
+++ TPC-H V3.0.1/dbgen/load_queries.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 for q in `seq 1 22`
 do
-    DSS_QUERY=../../pg_tpch/dss/templates ./qgen $q >> ./out_queries/$q.sql
-    sed 's/^select/explain select/' ./out_queries/$q.sql > ./out_queries/$q.explain.sql
-    cat ./out_queries/$q.sql >> ./out_queries/$q.explain.sql;
+    DSS_QUERY=/home/excalibur/learned-sql-optimizer/experimentation/templates ./qgen $q >> /home/excalibur/learned-sql-optimizer/experimentation/tpch_queries/$q.sql
+    sed 's/^select/explain select/' /home/excalibur/learned-sql-optimizer/experimentation/tpch_queries/$q.sql > /home/excalibur/learned-sql-optimizer/experimentation/tpch_queries/$q.explain.sql
+    cat /home/excalibur/learned-sql-optimizer/experimentation/tpch_queries/$q.sql >> /home/excalibur/learned-sql-optimizer/experimentation/tpch_queries/$q.explain.sql;
 done
---
+++ experimentation/tpch_queries/19.explain.sql
@@ -0,0 +1,77 @@
+-- using 1713647002 as a seed to the RNG
+
+
+explain select
+	sum(l_extendedprice* (1 - l_discount)) as revenue
+from
+	lineitem,
+	part
+where
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#53'
+		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
+		and l_quantity >= 10 and l_quantity <= 10 + 10
+		and p_size between 1 and 5
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+	or
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#32'
+		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
+		and l_quantity >= 13 and l_quantity <= 13 + 10
+		and p_size between 1 and 10
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+	or
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#41'
+		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
+		and l_quantity >= 23 and l_quantity <= 23 + 10
+		and p_size between 1 and 15
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+LIMIT ALL;-- using 1713647002 as a seed to the RNG
+
+
+select
+	sum(l_extendedprice* (1 - l_discount)) as revenue
+from
+	lineitem,
+	part
+where
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#53'
+		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
+		and l_quantity >= 10 and l_quantity <= 10 + 10
+		and p_size between 1 and 5
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+	or
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#32'
+		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
+		and l_quantity >= 13 and l_quantity <= 13 + 10
+		and p_size between 1 and 10
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+	or
+	(
+		p_partkey = l_partkey
+		and p_brand = 'Brand#41'
+		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
+		and l_quantity >= 23 and l_quantity <= 23 + 10
+		and p_size between 1 and 15
+		and l_shipmode in ('AIR', 'AIR REG')
+		and l_shipinstruct = 'DELIVER IN PERSON'
+	)
+LIMIT ALL;
